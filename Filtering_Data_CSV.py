96743fce7b0bf67a11dbd4a97e2c8d43c6eb5265
rohannair2022 <99500406+rohannair2022@users.noreply.github.com>
2022-08-14 14:36:11 +0530

    Delete Filtering_Data_CSV.py

--- Filtering_Data_CSV.py
+++
@@ -1,7 +0,0 @@
-#grades. csv includes grades of 12 kids
-
-folder = "C:\\Users\\Rohans PC\\OneDrive\\Desktop\\"
-import pandas as pd
-grades = pd.read_csv(folder + 'grades.csv')
-a = grades.head()
-print(a)
