3e59a61277b279329643a296ee1fb120fb772f26
rohannair2022 <99500406+rohannair2022@users.noreply.github.com>
2022-08-16 13:07:42 +0530

    Update displaying_csv_file.py

--- displaying_csv_file.py
+++ displaying_csv_file.py
@@ -1,5 +1,7 @@
 folder = "C:\\Users\\Rohans PC\\OneDrive\\Desktop\\" #path address of the file should always have double slash
 import pandas as pd
 grades = pd.read_csv(folder + 'grades.csv')
+
 grades.head() #used to display contents of the CSV file 
 grades.columns #used to display the column headers
+grades['column_name'] # used to show the contents of a specific column
