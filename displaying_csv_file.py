1a42662e8b189c3504ad239147b2b67748413545
rohannair2022 <99500406+rohannair2022@users.noreply.github.com>
2022-08-16 15:52:42 +0530

    Update displaying_csv_file.py

--- displaying_csv_file.py
+++ displaying_csv_file.py
@@ -5,3 +5,5 @@ grades = pd.read_csv(folder + 'grades.csv')
 grades.head() #used to display contents of the CSV file 
 grades.columns #used to display the column headers
 grades['column_name'] # used to show the contents of a specific column
+grades.loc[grades['column_name'] = 'item'] # used to locate a certain item in a column 
+grades.describe() # used to give a little statistic about the data 
