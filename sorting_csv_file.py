d4b66be54f7417bb89ffe2de73dcb419837799f5
rohannair2022 <99500406+rohannair2022@users.noreply.github.com>
2022-08-17 14:15:39 +0530

    Update sorting_csv_file.py

--- sorting_csv_file.py
+++ sorting_csv_file.py
@@ -1,3 +1,4 @@
 folder = "C:\\Users\\Rohans PC\\OneDrive\\Desktop\\" 
 import pandas as pd
 grades = pd.read_csv(folder + 'grades.csv')
+grades.sort_values(['column_name'], ascending = True) # sorts the data according to the ascending order of the column given 
