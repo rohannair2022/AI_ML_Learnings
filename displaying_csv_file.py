eed9393f8789125c4ecb99057267f7c00d486423
rohannair2022 <99500406+rohannair2022@users.noreply.github.com>
2022-08-19 16:06:36 +0530

    Update displaying_csv_file.py

--- displaying_csv_file.py
+++ displaying_csv_file.py
@@ -7,3 +7,4 @@ grades.columns #used to display the column headers
 grades['column_name'] # used to show the contents of a specific column
 grades.loc[grades['column_name'] = 'item'] # used to locate a certain item in a column 
 grades.describe() # used to give a little statistic about the data 
+
