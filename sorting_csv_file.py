553b801a54e4cdf8c09578f73ee43264958027e1
rohannair2022 <99500406+rohannair2022@users.noreply.github.com>
2022-08-19 17:08:55 +0530

    Update sorting_csv_file.py

--- sorting_csv_file.py
+++ sorting_csv_file.py
@@ -3,6 +3,7 @@ import pandas as pd
 grades = pd.read_csv(folder + 'grades.csv')
 grades.sort_values(['column_name'], ascending = True) # sorts the data according to the ascending order of the column given 
 grades.groupby(['column_name_1']).mean().sort_values('coulmn_name_2', ascending = False) # sort data based on the mean value of a certain column 
+grades.groupby(['column_name']).mean() #displays all the mean data accoding to data in a certain column
 
 df['count'] = 1
 grades.groupby(['column_name'].count()['count']) # used to count each item in a particular column
