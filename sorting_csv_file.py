0b086b07786e82be769d87c8dac98d4b914a87c6
rohannair2022 <99500406+rohannair2022@users.noreply.github.com>
2022-08-19 16:11:34 +0530

    Update sorting_csv_file.py

--- sorting_csv_file.py
+++ sorting_csv_file.py
@@ -5,6 +5,6 @@ grades.sort_values(['column_name'], ascending = True) # sorts the data according
 grades.groupby(['column_name_1']).mean().sort_values('coulmn_name_2', ascending = False) # sort data based on the mean value of a certain column 
 
 df['count'] = 1
-grades.groupby(['column_name'].count()['count'] # used to count each item in a particular column
-grades.groupby(['column_name1','column_name2'].count()['count'] #used to count each item in a particular column with respect to the other column as well
+grades.groupby(['column_name'].count()['count']) # used to count each item in a particular column
+grades.groupby(['column_name1','column_name2'].count()['count']) #used to count each item in a particular column with respect to the other column as well
          
